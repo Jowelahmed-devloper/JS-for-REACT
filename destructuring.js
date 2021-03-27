bd830a483918345c75e3aeb212f94cddb9f184b6
unknown <hello.jewelahmed@gmail.com>
2021-03-27 09:21:44 +0600

    Object Destructur

--- destructuring.js
+++ destructuring.js
@@ -24,3 +24,13 @@ function createArray(num2){
 }
 const [box1, box2] = createArray(3)
 console.log(box1, box2);
+// Object Destructuring
+
+const{name, age} = {id:1, name:'Rahim', age:23}
+// const{name, age} = {id:2, name:'Kahim', age:24}
+// const{name, age} = {id:3, name:'Jahim', age:25}
+console.log(name,age);
+
+// Object destructuring
+const {callingSymbol, city} ={callingSymbol:'Jowel AHmed', city:'savar'}
+console.log(callingSymbol,city);
