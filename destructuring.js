665c74c1063b6c04072173798e151be7649193c5
unknown <hello.jewelahmed@gmail.com>
2021-03-27 08:46:37 +0600

    smothing

--- destructuring.js
+++ destructuring.js
@@ -4,3 +4,16 @@ const numbers = [44, 54]
 // const [x , y] =[44, 54]
 const [x,y] = numbers
 console.log(x,y);
+
+// Make Array By Function
+ function boxify (num){
+     return [num, num+5]
+ }
+ const boxed = boxify(3)
+ console.log(boxed);
+// Make Array from a Function
+ function makeArray(num1){
+     return[num1, num1+4]
+ }
+ const arr = makeArray(10)
+ console.log(arr);
